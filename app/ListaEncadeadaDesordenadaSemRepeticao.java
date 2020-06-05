6e44d1572cedb1313f6a385c7dfda26c3fbbc8a7
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-06-05 16:11:52 -0300

    Mais métodos, autoformatação

--- app/ListaEncadeadaDesordenadaSemRepeticao.java
+++ app/ListaEncadeadaDesordenadaSemRepeticao.java
@@ -2,260 +2,337 @@ package app;
 
 import java.lang.reflect.*;
 
-public class ListaEncadeadaDesordenadaSemRepeticao<X>
-{
-    public class No
-    {
-        private No ante;
-        private X  info;
-        private No prox;
+public class ListaEncadeadaDesordenadaSemRepeticao<X> {
+	public class No {
+		private No ante;
+		private X info;
+		private No prox;
 
-        public No (No a, X i, No p)
-        {
+		public No(No a, X i, No p) {
 			this.ante = a;
-            this.info = i;
-            this.prox = p;
-        }
+			this.info = i;
+			this.prox = p;
+		}
 
-        public No (X i)
-        {
+		public No(X i) {
 			this.ante = null;
-            this.info = i;
-            this.prox = null;
-        }
+			this.info = i;
+			this.prox = null;
+		}
 
-        public No getAnte ()
-        {
-            return this.ante;
-        }
-        
-        public X getInfo ()
-        {
-            return this.info;
-        }
+		public No getAnte() {
+			return this.ante;
+		}
 
-        public No getProx ()
-        {
-            return this.prox;
-        }
+		public X getInfo() {
+			return this.info;
+		}
 
-        public void setAnte (No a)
-        {
-            this.ante = a;
-        }
-        
-        public void setInfo (X i)
-        {
-            this.info = i;
-        }
+		public No getProx() {
+			return this.prox;
+		}
 
-        public void setProx (No p)
-        {
-            this.prox = p;
-        }
-    }
+		public void setAnte(No a) {
+			this.ante = a;
+		}
 
-    protected No primeiro, ultimo;
+		public void setInfo(X i) {
+			this.info = i;
+		}
 
-    public ListaEncadeadaDesordenadaSemRepeticao()
-    {
-        this.primeiro = this.ultimo = null;
-    }
+		public void setProx(No p) {
+			this.prox = p;
+		}
+	}
 
-    protected X meuCloneDeX (X x)
-    {
-        X ret=null;
+	protected No primeiro, ultimo;
 
-        try
-        {
-            Class<?> classe = x.getClass();
-            Class<?>[] tiposDosParms = null;
-            Method metodo = classe.getMethod("clone",tiposDosParms);
-            Object[] parms = null;
-            ret = (X)metodo.invoke(x,parms);
-        }
-        catch (Exception erro)
-        {}
+	public ListaEncadeadaDesordenadaSemRepeticao() {
+		this.primeiro = this.ultimo = null;
+	}
 
-        return ret;
-    }
+	protected X meuCloneDeX(X x) {
+		X ret = null;
 
-    public void insiraNoInicio(X i) throws Exception
-    {
-        if (i == null)
-            throw new Exception("Infomacao passada nula");
+		try {
+			Class<?> classe = x.getClass();
+			Class<?>[] tiposDosParms = null;
+			Method metodo = classe.getMethod("clone", tiposDosParms);
+			Object[] parms = null;
+			ret = (X) metodo.invoke(x, parms);
+		} catch (Exception erro) {
+		}
 
-        if (this.tem(i))
-            throw new Exception("Informacao j� existente");
+		return ret;
+	}
 
-        X inserir=null;
-        if (i instanceof Cloneable)
-            inserir = meuCloneDeX (i);
-        else
-            inserir = i;
-            
-        this.primeiro = new No (null,inserir,this.primeiro);
+	public void insiraNoInicio(X i) throws Exception {
+		if (i == null)
+			throw new Exception("Infomacao passada nula");
 
-        if (this.primeiro.getProx()!=null)
-            this.primeiro.getProx().setAnte (this.primeiro);
+		if (this.tem(i))
+			throw new Exception("Informacao j� existente");
 
-        this.primeiro.setAnte (null);
-        
-        if (this.ultimo==null)
-            this.ultimo=this.primeiro;
-    }
+		X inserir = null;
+		if (i instanceof Cloneable)
+			inserir = meuCloneDeX(i);
+		else
+			inserir = i;
 
-    public void insiraNoFim(X i) throws Exception
-    {
-        if (i==null)
-            throw new Exception ("Informacao passada nula");
+		this.primeiro = new No(null, inserir, this.primeiro);
 
-        if (this.tem(i))
-            throw new Exception("Informacao ja existente");
+		if (this.primeiro.getProx() != null)
+			this.primeiro.getProx().setAnte(this.primeiro);
 
-        X inserir=null;
-        if (i instanceof Cloneable)
-            inserir = meuCloneDeX (i);
-        else
-            inserir = i;
-            
-        if (this.ultimo==null)
-        {
-            this.ultimo   = new No (inserir);
-            this.primeiro = this.ultimo;
-        }
-        else
-        {
-            this.ultimo.setProx (new No (this.ultimo,inserir,null));
-            this.ultimo = this.ultimo.getProx();
-        }
-    }
+		this.primeiro.setAnte(null);
 
-    // !TODO REMOVAFIM
+		if (this.ultimo == null)
+			this.ultimo = this.primeiro;
+	}
 
-    // !TODO REMOVAINICIO 
-    
-    public void remova (X i) throws Exception
-    {
-        if (i==null)
-            throw new Exception ("Informacao passada nula");
+	public void insiraNoFim(X i) throws Exception {
+		if (i == null)
+			throw new Exception("Informacao passada nula");
 
-        if (this.primeiro==null)
-            throw new Exception ("A lista esta vazia");
+		if (this.tem(i))
+			throw new Exception("Informacao ja existente");
 
-        if (i.equals(this.primeiro.getInfo()))
-        {
+		X inserir = null;
+		if (i instanceof Cloneable)
+			inserir = meuCloneDeX(i);
+		else
+			inserir = i;
+
+		if (this.ultimo == null) {
+			this.ultimo = new No(inserir);
+			this.primeiro = this.ultimo;
+		} else {
+			this.ultimo.setProx(new No(this.ultimo, inserir, null));
+			this.ultimo = this.ultimo.getProx();
+		}
+	}
+
+	public void removaDoFim() throws Exception {
+		if (this.isVazia())
+			throw new Exception("A lista esta vazia");
+
+		this.ultimo = this.ultimo.getAnte();
+		this.ultimo.setProx(null);
+
+	}
+
+	public void removaDoInicio() throws Exception {
+		if (this.isVazia())
+			throw new Exception("A lista esta vazia");
+
+		this.primeiro = this.primeiro.getProx();
+		this.primeiro.setAnte(null);
+	}
+
+	public void remova(X i) throws Exception {
+		if (i == null)
+			throw new Exception("Informacao passada nula");
+
+		if (this.isVazia())
+			throw new Exception("A lista esta vazia");
+
+		if (i.equals(this.primeiro.getInfo())) {
 			this.primeiro = this.primeiro.getProx();
 
-			if (this.primeiro==null)
+			if (this.primeiro == null)
 				this.ultimo = null;
 			else
 				this.primeiro.setAnte(null);
-				
+
 			return;
-        }
+		}
 
-        No atual=this.primeiro;
+		No atual = this.primeiro;
 
-        for(;;)
-        {
-            if (atual==null)
-                throw new Exception ("Lista esta� vazia (Esse erro nao deveria acontecer)");
+		for (;;) {
+			if (atual == null)
+				throw new Exception("Lista esta vazia (Esse erro nao deveria acontecer)");
 
-            if (i.equals(atual.getInfo()))
-            {
-                // ### ATUAL NAO SERA O PRIMEIRO ###
-				if (atual==this.primeiro)            
-				{
+			if (i.equals(atual.getInfo())) {
+				// ### ATUAL NAO SERA O PRIMEIRO ###
+				if (atual == this.primeiro) {
 					this.primeiro = this.primeiro.getProx();
 
-					if (this.primeiro==null) 
+					if (this.primeiro == null)
 						this.ultimo = null;
 					else
-						this.primeiro.setAnte (null);
-						
+						this.primeiro.setAnte(null);
+
 					return;
 				}
-				
-				if (atual==this.ultimo)
-				{
+
+				if (atual == this.ultimo) {
 					this.ultimo = this.ultimo.getAnte();
 
-					if (this.ultimo==null)
-						this.primeiro=null;
+					if (this.ultimo == null)
+						this.primeiro = null;
 					else
 						this.ultimo.setProx(null);
-						
+
 					return;
 				}
-				
+
 				atual.getAnte().setProx(atual.getProx());
 				atual.getProx().setAnte(atual.getAnte());
-                return;
-            }
+				return;
+			}
 
-            atual=atual.getProx();
-        }
-    }
+			atual = atual.getProx();
+		}
+	}
 
-    public boolean tem (X i) throws Exception
-    {
-		if (i==null)
-		    throw new Exception ("Informacao ausente");
-		
-        No atual=this.primeiro;
+	public boolean tem(X i) throws Exception {
+		if (i == null)
+			throw new Exception("Informacao ausente");
 
-        while (atual!=null)
-        {
-            if (i.equals(atual.getInfo()))
-                return true;
-                
-            atual = atual.getProx();
-        }
-        
-        return false;
-    }
+		No atual = this.primeiro;
 
-    // !TODO GET QTD
-    
-    public X getDoInicio () throws Exception
-    {
-        if (this.primeiro==null)
-            throw new Exception ("Lista vazia, nada a obter");
+		while (atual != null) {
+			if (i.equals(atual.getInfo()))
+				return true;
 
-        X ret = this.primeiro.getInfo();
-        if (ret instanceof Cloneable)
-            ret = meuCloneDeX (ret);
-            
-        return ret;
-    }
-    
-    // !TODO GETDOFIM
+			atual = atual.getProx();
+		}
 
-    // !TODO ISVAZIA
+		return false;
+	}
 
-    // !TODO INVERTASE
+	public int getQtd() {
+		int ret = 0;
 
-    // !TODO INVERSAO
+		No atual = this.primeiro;
 
-    public String toString()
-    {
-        String ret="[";
+		while (atual != null) {
+			ret++;
+			atual = atual.getProx();
+		}
 
-        No atual=this.primeiro;
+		return ret;
+	}
 
-        while (atual!=null)
-        {
-            ret=ret+atual.getInfo();
+	public X getPrimeiro() throws Exception {
+		if (this.isVazia())
+			throw new Exception("Lista vazia, nada a obter");
 
-            if (atual!=this.ultimo)
-                ret=ret+",";
+		X ret = this.primeiro.getInfo();
+		if (ret instanceof Cloneable)
+			ret = meuCloneDeX(ret);
 
-            atual=atual.getProx();
-        }
+		return ret;
+	}
 
-        return ret+"]";
-    }
+	public X getUltimo() throws Exception {
+		if (this.isVazia())
+			throw new Exception("Lista vazia, nada a obter");
 
-    // !TODO METODOS OBRIGATORIO(JA FEITO: toString())
+		X ret = this.ultimo.getInfo();
+		if (ret instanceof Cloneable)
+			ret = meuCloneDeX(ret);
+
+		return ret;
+	}
+
+	public boolean isVazia() {
+		return this.primeiro == null;
+	}
+
+	// !TODO INVERTASE
+
+	// !TODO INVERSAO
+
+	public String toString() {
+		String ret = "[";
+
+		No atual = this.primeiro;
+
+		while (atual != null) {
+			ret = ret + atual.getInfo();
+
+			if (atual != this.ultimo)
+				ret = ret + ", ";
+
+			atual = atual.getProx();
+		}
+
+		return ret + "]";
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (this.getClass() != obj.getClass())
+			return false;
+
+		ListaEncadeadaDesordenadaSemRepeticao<X> outro = (ListaEncadeadaDesordenadaSemRepeticao<X>) obj;
+
+		No thisNo = this.primeiro;
+		No outroNo = outro.primeiro;
+
+		while (thisNo != null && outroNo != null) {
+			if (!thisNo.getInfo().equals(outroNo.getInfo()))
+				return false;
+
+			thisNo = thisNo.getProx();
+			outroNo = outroNo.getProx();
+		}
+
+		if (thisNo != null || outroNo != null)
+			return false;
+
+		return true;
+	}
+
+	public int hashCode() {
+		int ret = 7;
+
+		No atual = this.primeiro;
+
+		while (atual != null) {
+			ret = 13 * ret + atual.hashCode();
+			atual = atual.getProx();
+		}
+
+		return ret;
+	}
+
+	public ListaEncadeadaDesordenadaSemRepeticao(ListaEncadeadaDesordenadaSemRepeticao<X> modelo) throws Exception {
+		if (modelo == null)
+			throw new Exception("Modelo ausente");
+
+		if (modelo.primeiro == null)
+			return;
+
+		this.primeiro = new No(modelo.primeiro.getInfo());
+
+		No thisNo = this.primeiro;
+		No outroNo = modelo.primeiro.getProx();
+
+		while (outroNo != null) {
+			thisNo.setProx(new No(thisNo, outroNo.getInfo(), null));
+			thisNo = thisNo.getProx();
+			outroNo = outroNo.getProx();
+		}
+
+		this.ultimo = thisNo;
+	}
+
+	public Object clone() {
+		ListaEncadeadaDesordenadaSemRepeticao ret = null;
+
+		try {
+			ret = new ListaEncadeadaDesordenadaSemRepeticao(this);
+		} catch (Exception e) {
+		}
+
+		return ret;
+	}
 }
--- test/TestList.java
+++ test/TestList.java
@@ -2,39 +2,53 @@ package test;
 
 import app.ListaEncadeadaDesordenadaSemRepeticao;
 
-public class TestList
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            ListaEncadeadaDesordenadaSemRepeticao<String> lista = new ListaEncadeadaDesordenadaSemRepeticao<String> ();
+public class TestList {
+	public static void main(String[] args) {
+		try {
+			ListaEncadeadaDesordenadaSemRepeticao<String> lista = new ListaEncadeadaDesordenadaSemRepeticao<String>();
 
-            System.out.println(lista.toString());
-            
-            System.out.println("insiraNoInicio(\"def\")");
-            lista.insiraNoInicio("def");
-            
-            System.out.println(lista.toString());
-            
-            System.out.println("insiraNoFim(\"uvw\")");
-            lista.insiraNoFim("ghi");
-            
-            System.out.println(lista.toString());
-            
-            System.out.println("insiraNoFim(\"xyz\")");
-            lista.insiraNoFim("jkl");
-            
-            System.out.println(lista.toString());
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
 
-            System.out.println("insiraNoInicio(\"abc\")");
-            lista.insiraNoInicio("abc");
-            
-            System.out.println(lista.toString());
-        }
-        catch (Exception ex)
-        {
-            System.err.println(ex.getMessage());
-        }
-    }
+			System.out.println("insiraNoInicio(\"def\")");
+			lista.insiraNoInicio("def");
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println("insiraNoFim(\"uvw\")");
+			lista.insiraNoFim("ghi");
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println("insiraNoFim(\"xyz\")");
+			lista.insiraNoFim("jkl");
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println("insiraNoInicio(\"abc\")");
+			lista.insiraNoInicio("abc");
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println("removaDoFim()");
+			lista.removaDoFim();
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println("removaDoInicio()");
+			lista.removaDoInicio();
+
+			System.out.println(lista.toString() + " qtd: " + lista.getQtd() + "\n");
+
+			System.out.println();
+
+			System.out.print("getPrimeiro(): ");
+			System.out.println(lista.getPrimeiro());
+
+			System.out.print("getUltimo(): ");
+			System.out.println(lista.getUltimo());
+
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
 }
